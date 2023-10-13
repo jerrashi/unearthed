bdaf8e7d7abf7af793fb168c4d036484971c78a9
Jerry Shi <jerryshi25@gmail.com>
2023-10-13 18:32:27 -0400

    setup controller and routers for CRUD operations

--- server/controllers/gifts.js
+++ server/controllers/gifts.js
@@ -23,6 +23,52 @@ const getGiftById = async (req, res) => {
   }
 }
 
+const createGift = async (req, res) => {
+  try{
+    const { name, pricePoint, audience, image, description, submittedBy, submittedOn } = req.body
+    const results = await pool.query(`
+      INSERT INTO gifts (name, pricepoint, audience, image, description, submittedby, submittedon)
+      VALUES($1, $2, $3, $4, $5, $6, $7)
+      RETURNING *`,
+      [name, pricePoint, audience, image, description, submittedBy, submittedOn])
+    res.status(201).json(results.rows[0])
+  } 
+  catch (error) {
+    res.status(409).json( { error: error.message} )
+  } 
+} 
+
+const updateGift = async(req, res) => {
+  try{
+    const giftId = req.params.giftId
+    const { name, pricePoint, audience, image, description, submittedBy, submittedOn } = req.body
+    const results = await pool.query(`
+      UPDATE gifts
+      SET name = $1, pricepoint = $2, audience = $3, image = $4, description = $5, submittedby = $6, submittedon = $7
+      WHERE id = $8
+      RETURNING *`,
+      [name, pricePoint, audience, image, description, submittedBy, submittedOn, giftId])
+    res.status(200).json(results.rows[0])
+  }
+  catch (error){
+    res.status(406).json( { error: error.message} ) 
+  }
+}
+
+const deleteGift = async(req, res) => {
+  try{
+    const giftId = req.params.giftId
+    const results = await pool.query(`
+      DELETE FROM gifts
+      WHERE id = $1`,
+      [giftId])
+    res.status(200).json(results.rows[0])
+  }
+  catch (error){
+    res.status(406).json( { error: error.message} ) 
+  }
+}
+
 export default {
-  getGifts, getGiftById
+  getGifts, getGiftById, createGift, updateGift, deleteGift
 }
--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -6,6 +6,12 @@ const router = express.Router()
 
 router.get('/', GiftsController.getGifts)
 
+router.post('/', GiftsController.createGift)
+
 router.get('/:giftId', GiftsController.getGiftById)
+
+router.delete('/:giftId', GiftsController.deleteGift)
+
+router.patch('/:giftId', GiftsController.updateGift)
 
 export default router
