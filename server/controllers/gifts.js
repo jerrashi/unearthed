0efc05973b8a7b7af742f69e8bd0c0a12f309ed9
Jerry Shi <jerryshi25@gmail.com>
2023-10-14 13:55:39 -0400

    replaced single tick w backtick

--- server/controllers/gifts.js
+++ server/controllers/gifts.js
@@ -2,7 +2,7 @@ import { pool } from '../config/database.js'
 
 const getGifts = async (req, res) => {
   try {
-    const results = await pool.query('SELECT * FROM gifts ORDER BY id ASC')
+    const results = await pool.query(`SELECT * FROM gifts ORDER BY id ASC`)
     res.status(200).json(results.rows)
   } catch (error) {
     res.status(400).json( { error: error.message } )
